d1643aeb6af1f39ffe7e4d02c1cdc7a03e88410f
Apalakimmortal <apalakimmortal045@gmail.com>
2024-07-09 14:48:09 +0530

    UI implemented using Ejs and server side rendering done

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const express = require("express");
 const {connectToMongoDB} = require("./connect")
+const path = require("path")
 const urlRoute = require("./routes/url")
+const staticRoute = require("./routes/staticRouter")
 const URL = require("./models/url")
 
 const app = express();
 const PORT = 8001;
 
 connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
-console.log("MongoDB Connected")
+    console.log("MongoDB Connected")
 );
 
+
+app.set('view engine', 'ejs')
+app.set('views', path.resolve('./views'))
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+
+//server side rendering
+// app.get("/test",async(req,res) =>{
+//     const allUrls = await URL.find({});
+//     //you can also send objects
+//     return res.render("home" , {
+//         urls:allUrls,
+//     })
+// })
+
+
+app.use("/",staticRoute);
 
 app.use("/url",urlRoute);
 
